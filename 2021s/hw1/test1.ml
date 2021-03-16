de14d1ae8b71b1f827a025de9ff7968fd4e375e7
Final Child <finalchild2@gmail.com>
2021-03-16 11:35:33 +0900

    Use old hw1/test1

--- 2021s/hw1/test1.ml
+++ 2021s/hw1/test1.ml
@@ -5,28 +5,31 @@ open Testlib
 module TestEx1: TestEx =
   struct
     type testcase =
-        MERGE of int list * int list * int list
+      | MERGE of int list * int list * int list
 
     let testcases: testcase list =
-      [ MERGE ([], [], [])
-      ; MERGE ([], [-1;-300], [-1;-300])
-      ; MERGE ([-1;-300], [], [-1;-300])
-      ; MERGE ([130;5;4;2], [3;1;-1], [130;5;4;3;2;1;-1])
-      ; MERGE ([130;5;4;2], [3;1;-1;-3000], [130;5;4;3;2;1;-1;-3000])
+      [ MERGE ([3;2;1], [6;5;4], [6;5;4;3;2;1])
+      ; MERGE ([-1;-2;-3;-4;-5], [5;3;0;-6], [5;3;0;-1;-2;-3;-4;-5;-6])
+      ; MERGE ([], [], [])
+      ; MERGE ([1], [], [1])
+      ; MERGE ([], [1], [1])
+      ; MERGE ([5;4;3;2;1], [], [5;4;3;2;1])
+      ; MERGE ([], [5;4;3;2;1], [5;4;3;2;1])
+      ; MERGE ([10;8;6;4;2;0;-2;-4;-6;-8;-10], [9;7;5;3;1;-1;-3;-5;-7;-9], [10;9;8;7;6;5;4;3;2;1;0;-1;-2;-3;-4;-5;-6;-7;-8;-9;-10])
       ]
 
     let runner (tc: testcase): bool =
       match tc with
-      | MERGE (a, b, ans) -> merge(a,b) = ans
-
-    let string_of_int_list: int list -> string = string_of_list string_of_int
+      | MERGE (l1, l2, ans) -> merge(l1, l2) = ans
 
     let string_of_tc (tc: testcase): string * string * string =
       match tc with
-      | MERGE (a, b, ans) ->
-          ( Printf.sprintf "MERGE(%s, %s, %s)" (string_of_int_list a) (string_of_int_list b) (string_of_int_list ans)
+      | MERGE (l1, l2, ans) ->
+          let string_of_int_list = string_of_list string_of_int in
+          let output = merge(l1, l2) in
+          ( Printf.sprintf "merge(%s, %s)" (string_of_int_list l1) (string_of_int_list l2)
           , string_of_int_list ans
-          , string_of_int_list (merge(a,b))
+          , string_of_int_list output
           )
   end
 
